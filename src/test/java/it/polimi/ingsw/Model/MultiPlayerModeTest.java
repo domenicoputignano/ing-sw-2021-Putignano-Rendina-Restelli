b355311cb8d1d4ea388c9814c882fa9a0c8cd459
domenicoputignano <domenico1.putignano@mail.polimi.it>
2021-05-22 17:00:02 +0200

    Tested concludeGame in MultiPlayerModeTest class

--- src/test/java/it/polimi/ingsw/Model/MultiPlayerModeTest.java
+++ src/test/java/it/polimi/ingsw/Model/MultiPlayerModeTest.java
@@ -1,10 +1,11 @@
 package it.polimi.ingsw.Model;
 
-import it.polimi.ingsw.Commons.StateFavorTiles;
+import it.polimi.ingsw.Commons.*;
+import it.polimi.ingsw.Exceptions.DepotOutOfBoundsException;
+import it.polimi.ingsw.Exceptions.IncompatibleResourceTypeException;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -66,4 +67,69 @@ class MultiPlayerModeTest {
         playerList.get(2).getUser().setActive(false);
         System.out.println(multiPlayerMode.nextPlayer(multiPlayerMode.getCurrPlayer()));
     }
+    @Test
+    void concludeGame() throws DepotOutOfBoundsException, IncompatibleResourceTypeException {
+        playerList.add(new Player("Piero"));
+        playerList.add(new Player("Domenico"));
+        playerList.add(new Player("Andrea"));
+        playerList.add(new Player("Mario"));
+        MultiPlayerMode multiPlayerMode = new MultiPlayerMode(playerList.get(0),playerList,playerList.get(0),playerList.size());
+        multiPlayerMode.setup();
+        Map<ResourceType,Integer> startingresource = new EnumMap<ResourceType, Integer>(ResourceType.class);
+
+        for(int i=0;i<4;i++) {
+            playerList.get(i).getPersonalBoard().getWarehouse().addResourcesToDepot(3, ResourceType.shield, 3);
+            playerList.get(i).getPersonalBoard().getWarehouse().addResourcesToDepot(2, ResourceType.coin, 2);
+            playerList.get(i).getPersonalBoard().getWarehouse().addResourcesToDepot(1, ResourceType.stone, 1);
+            startingresource.put(ResourceType.servant, 2);
+            startingresource.put(ResourceType.coin, 3);
+            startingresource.put(ResourceType.stone, 4);
+            startingresource.put(ResourceType.shield, 1);
+            playerList.get(i).getPersonalBoard().getWarehouse().addResourcesToStrongbox(startingresource);
+        }
+        playerList.get(0).getLeaderCards().get(0).setIsActive();
+        playerList.get(1).getLeaderCards().get(0).setIsActive();
+        playerList.get(1).getLeaderCards().get(1).setIsActive();
+        playerList.get(2).getLeaderCards().get(0).setIsActive();
+        playerList.get(2).getLeaderCards().get(1).setIsActive();
+        playerList.get(3).getLeaderCards().get(0).setIsActive();
+
+        playerList.get(0).getPersonalBoard().getFaithTrack().moveMarker(8);
+        playerList.get(3).getPersonalBoard().getFaithTrack().moveMarker(11);
+        playerList.get(1).getPersonalBoard().getFaithTrack().moveMarker(5);
+        playerList.get(2).getPersonalBoard().getFaithTrack().moveMarker(15);
+
+
+
+        Map<ResourceType, Integer> cost = new EnumMap<ResourceType, Integer>(ResourceType.class);
+        cost.put(ResourceType.servant,0);
+        cost.put(ResourceType.coin,0);
+        cost.put(ResourceType.stone,0);
+        cost.put(ResourceType.shield,0);
+        playerList.get(0).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,1, ColorCard.blue,3,null),1);
+        playerList.get(0).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,2,ColorCard.yellow,5,null),1);
+        playerList.get(0).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,1,ColorCard.purple,2,null),2);
+        playerList.get(0).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,1,ColorCard.green,3,null),3);
+
+        playerList.get(1).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,1, ColorCard.blue,2,null),1);
+        playerList.get(1).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,2,ColorCard.yellow,4,null),1);
+
+        playerList.get(2).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,1, ColorCard.blue,1,null),1);
+        playerList.get(2).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,2,ColorCard.yellow,2,null),1);
+        playerList.get(2).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,1, ColorCard.blue,4,null),2);
+
+        playerList.get(3).getPersonalBoard().putCardOnTop(new DevelopmentCard(cost,1, ColorCard.blue,3,null),1);
+
+        int victorypoints1 = playerList.get(0).calcVictoryPointsPlayer();
+        int victorypoints2 = playerList.get(1).calcVictoryPointsPlayer();
+        int victorypoints3 = playerList.get(2).calcVictoryPointsPlayer();
+        int victorypoints4 = playerList.get(3).calcVictoryPointsPlayer();
+
+        System.out.println("1) "+victorypoints1);
+        System.out.println("2) "+victorypoints2);
+        System.out.println("3) "+victorypoints3);
+        System.out.println("4) "+victorypoints4);
+
+        multiPlayerMode.concludeGame();
+       }
 }
