1bba7ee3d9d50904d2d16416005562a52713417b
PieroRendina <piero.rendina@mail.polimi.it>
2021-05-31 12:03:26 +0200

    Added controller test

--- src/test/java/it/polimi/ingsw/Controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/Controller/GameControllerTest.java
@@ -1,12 +1,19 @@
 package it.polimi.ingsw.Controller;
 
 
+import it.polimi.ingsw.Commons.ResourceType;
+import it.polimi.ingsw.Model.Game;
+import it.polimi.ingsw.Model.GameState;
 import it.polimi.ingsw.Model.MultiPlayerMode;
 import it.polimi.ingsw.Model.Player;
 import it.polimi.ingsw.Model.SoloMode.SoloMode;
+import it.polimi.ingsw.Network.Client;
 import it.polimi.ingsw.Network.ClientStatus;
 import it.polimi.ingsw.Network.RemoteView;
 import it.polimi.ingsw.Utils.Messages.ClientMessages.LeaderChoiceMessage;
+import it.polimi.ingsw.Utils.Messages.ClientMessages.ResourceChoiceMessage;
+import it.polimi.ingsw.Utils.Pair;
+import org.apache.maven.model.Resource;
 import org.junit.jupiter.api.Test;
 
 
@@ -22,6 +29,35 @@ class GameControllerTest {
 
     @Test
     void handleResourceChoiceMessage() {
+        List<Player> players = new ArrayList<>();
+        Player first = spy(new Player("Piero"));
+        Player second = spy(new Player("Andrea"));
+        players.add(first);
+        players.add(second);
+        MultiPlayerMode multiPlayerMatch = new MultiPlayerMode(players);
+        multiPlayerMatch.nextState(GameState.RESOURCECHOICE);
+
+        MultiPlayerMode multiPlayerModeSpy = spy(multiPlayerMatch);
+
+        ClientStatus clientStatus = mock(ClientStatus.class);
+
+        GameController controller = new GameController(multiPlayerModeSpy);
+
+        RemoteView remoteView = new RemoteView(second.getUser(),controller,clientStatus);
+        RemoteView spyRemoteView = spy(remoteView);
+
+        List<Pair<ResourceType,Integer>> resources = new ArrayList<>();
+        resources.add(new Pair<>(ResourceType.coin,1));
+
+        ResourceChoiceMessage message = new ResourceChoiceMessage(resources);
+        ResourceChoiceMessage spyMessage = spy(message);
+
+        controller.handleResourceChoiceMessage(spyMessage, spyRemoteView);
+
+        verify(multiPlayerModeSpy, times(1)).getGameState();
+        when(spyMessage.getChosenResources()).thenReturn(resources);
+        verify(second, times(1)).performInitialResourcesChoice(multiPlayerModeSpy, resources);
+
     }
 
     @Test
