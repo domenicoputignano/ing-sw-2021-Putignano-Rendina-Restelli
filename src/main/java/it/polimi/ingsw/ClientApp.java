14c5a0faef37fdb4f6427d8c3affc891841ca38e
Andrea Restelli <andrearestelli28@gmail.com>
2021-06-01 09:31:03 +0200

    added command line arguments parsing in ClientApp

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -1,21 +1,25 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.Client.view.GUI.GUIApp;
 import it.polimi.ingsw.Network.Client;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class ClientApp {
 
-    private static boolean startAsGui = false;
-
     public static void main(String[] args) {
         Client client = new Client("127.0.0.1", 12345);
-       try{
-           client.start(startAsGui);
+        boolean startAsGui = parseArgs(args);
+        try{
+            client.start(startAsGui);
         }catch (IOException e){
             System.err.println(e.getMessage());
         }
+    }
+
+    private static boolean parseArgs(String[] args){
+        String interfaceChoice = args.length > 0 ? args[0].toUpperCase(Locale.ROOT) : "GUI";
+        return !"CLI".equals(interfaceChoice);
     }
 
 }
