d7e13afac25a5b5986653384b49c0b0dd46e5f46
domenicoputignano <domenico1.putignano@mail.polimi.it>
2021-06-25 17:49:57 +0200

    added javadoc of GUI

--- src/main/java/it/polimi/ingsw/Client/view/GUI/LobbyController.java
+++ src/main/java/it/polimi/ingsw/Client/view/GUI/LobbyController.java
@@ -8,22 +8,43 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
+/**
+ * Class that represents the controller of the Lobby page of the game
+ * On this page the user will have to wait for the number of players
+ * chosen in the previous step to connect to the server to play the game
+ */
 public class LobbyController extends Controller {
+    /**
+     * Attribute that represents the textField where the title of the page is shown
+     */
     @FXML
     public Text lobbyText;
-
+    /**
+     * Attribute that represents the textField where the first player in lobby is shown
+     */
     @FXML
     public TextField player1;
-
+    /**
+     * Attribute that represents the textField where the second player in lobby is shown
+     */
     @FXML
     public TextField player2;
-
+    /**
+     * Attribute that represents the textField where the third player in lobby is shown
+     */
     @FXML
     public TextField player3;
-
+    /**
+     * Attribute that represents the textField where the fourth player in lobby is shown
+     */
     @FXML
     public TextField player4;
 
+    /**
+     * Main method that initializes the scene within the stage
+     * It takes care of setting the background of the scene
+     * and the font of the texts and buttons
+     */
     @FXML
     public void initialize() {
         super.initialize();
@@ -46,19 +67,34 @@ public class LobbyController extends Controller {
         player4.setStyle("-fx-text-fill: rgb(35, 25, 22);");
         setFont(player4,30);
     }
-
+    /**
+     * method used to set the player username in the first textField of Lobby page
+     * @param player String that represents the player username to show in the TextField
+     */
     public void setPlayer1(String player)
     {
         player1.setText(player);
     }
+    /**
+     * method used to set the player username in the second textField of Lobby page
+     * @param player String that represents the player username to show in the TextField
+     */
     public void setPlayer2(String player)
     {
         player2.setText(player);
     }
+    /**
+     * method used to set the player username in the third textField of Lobby page
+     * @param player String that represents the player username to show in the TextField
+     */
     public void setPlayer3(String player)
     {
         player3.setText(player);
     }
+    /**
+     * method used to set the player username in the fourth textField of Lobby page
+     * @param player String that represents the player username to show in the TextField
+     */
     public void setPlayer4(String player)
     {
         player4.setText(player);
--- src/main/java/it/polimi/ingsw/Client/view/GUI/SelectNumOfPlayersController.java
+++ src/main/java/it/polimi/ingsw/Client/view/GUI/SelectNumOfPlayersController.java
@@ -7,21 +7,41 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-
+/**
+ * Class that represents the controller of the SelectNumbersOfPlayers page of the game
+ * On this page the user must choose the number of players he wants to play with
+ * Once the choice has been made, the user will be placed in a lobby
+ * waiting for the number of chosen players to connect
+ */
 public class SelectNumOfPlayersController extends Controller{
+    /**
+     * Attribute that represents the textField where the title of the page is shown
+     */
     @FXML
     public TextField numberPlayers;
-
+    /**
+     * Attribute that represents the button to press to choose two-player mode
+     */
     @FXML
     public Button twoPlayers;
 
+    /**
+     * Attribute that represents the button to press to choose three-player mode
+     */
     @FXML
     public Button threePlayers;
 
+    /**
+     * Attribute that represents the button to press to choose four-player mode
+     */
     @FXML
     public Button fourPlayers;
 
-
+    /**
+     * Main method that initializes the scene within the stage
+     * It takes care of setting the background of the scene
+     * and the font of the texts and buttons
+     */
     @FXML
     public void initialize() {
         super.initialize();
@@ -42,19 +62,30 @@ public class SelectNumOfPlayersController extends Controller{
         setFont(threePlayers,26);
         setFont(fourPlayers,26);
     }
-
+    /**
+     * method used to manage the user's choice to two-player mode
+     * It is performed when the twoPlayers button is pressed
+     */
     @FXML
     void handleTwoPlayersChoice() {
         clientState = new NumOfPlayerChoiceGUI(client, 2);
         clientState.manageUserInteraction();
         GUIApp.showScene("/gui/fxml/LobbyPage.fxml");
     }
+    /**
+     * method used to manage the user's choice to three-player mode
+     * It is performed when the threePlayers button is pressed
+     */
     @FXML
     void handleThreePlayersChoice() {
         clientState = new NumOfPlayerChoiceGUI(client, 3);
         clientState.manageUserInteraction();
         GUIApp.showScene("/gui/fxml/LobbyPage.fxml");
     }
+    /**
+     * method used to manage the user's choice to four-player mode
+     * It is performed when the fourPlayers button is pressed
+     */
     @FXML
     void handleFourPlayersChoice() {
         clientState = new NumOfPlayerChoiceGUI(client, 4);
